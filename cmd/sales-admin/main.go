e4e8ce6888b8fa932c80e433074fa5e3a7cd5417
runlevl4 <root@runlevl4.com>
2020-05-17 17:43:31 -0400

    Refactored to use config; added func run() to ensure clean shutdown in the event of os.Exit

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -2,37 +2,76 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/pkg/errors"
+	"github.com/runlevl4/garagesale/internal/platform/conf"
 	"github.com/runlevl4/garagesale/internal/platform/database"
 	"github.com/runlevl4/garagesale/internal/schema"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Printf("error | shutting down | %s", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+
+	var cfg struct {
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:localhost"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+		Args conf.Args
+	}
+
+	// =========================================================================
+	// Get configuration
+	if err := conf.Parse(os.Args[1:], "sales", &cfg); err != nil {
+		// Allows user to display help
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		}
+		return errors.Wrap(err, "parsing config")
+	}
 
 	// =========================================================================
 	// Setup database
-	db, err := database.Open()
+	db, err := database.Open(database.Config{
+		Host:       cfg.DB.Host,
+		User:       cfg.DB.User,
+		Password:   cfg.DB.Password,
+		Name:       cfg.DB.Name,
+		DisableTLS: cfg.DB.DisableTLS,
+	})
 	if err != nil {
-		log.Fatal(err)
+		return errors.Wrap(err, "error opening database")
 	}
 	defer db.Close()
 
-	flag.Parse()
-	switch flag.Arg(0) {
+	switch cfg.Args.Num(0) {
 	case "migrate":
 		if err := schema.Migrate(db); err != nil {
-			log.Fatal(err)
+			return errors.Wrap(err, "migrating database")
 		}
 		log.Println("main : migrate complete")
-		return
 	case "seed":
 		if err := schema.Seed(db); err != nil {
-			log.Fatal(err)
+			return errors.Wrap(err, "seeding database")
 		}
 		log.Println("main : seed complete")
-		return
 	}
-
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
+	github.com/google/go-cmp v0.4.1 // indirect
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.0.0
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/errors v0.9.1
 )
