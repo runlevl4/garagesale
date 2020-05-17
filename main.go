ba192abf34760ed410b8051653e8bb217556f378
runlevl4 <root@runlevl4.com>
2020-05-17 12:12:19 -0400

    Added flag support to manage db migration/seeding

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
+	"github.com/runlevl4/garagesale/schema"
 	"log"
 	"net/http"
 	"net/url"
@@ -24,6 +26,22 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	flag.Parse()
+	switch flag.Arg(0) {
+	case "migrate":
+		if err := schema.Migrate(db); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("main : migrate complete")
+		return
+	case "seed":
+		if err := schema.Seed(db); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("main : seed complete")
+		return
+	}
 
 	// =========================================================================
 	// App Starting
